d60c521edbc0807bb3d5097e0a9aef5452b51994
Mostafa Norzade <mostafanorzade@yahoo.com>
2017-11-20 12:40:37 +0330

    Option Tree

---
+++ option-tree.php
@@ -0,0 +1,19 @@
+<?php
+
+//===================================================================
+//------------- Add below code to functions.php ---------------------
+//------------- Documentation = https://www.sitepoint.com/optiontree-theme-options-ui-builder
+//===================================================================
+
+
+
+load_theme_textdomain( 'option-tree', get_template_directory() . '/option-tree/languages' );
+
+add_filter( 'ot_theme_mode', '__return_true' );
+require( trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php' );
+require( trailingslashit( get_template_directory() ) . 'option-tree/theme-options.php' );
+require( trailingslashit( get_template_directory() ) . 'option-tree/meta-boxes.php' );
+function custom_internal_css() {
+echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_url').'/option-tree/ot-custom-style.css" />';
+}
+add_action('admin_head', 'custom_internal_css');
