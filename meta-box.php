1a93008a288c286cb864ab05a723a85ebe6bb049
Mostafa Norzade <mostafanorzade@yahoo.com>
2017-11-20 13:46:33 +0330

    edit meta

--- meta-box.php
+++ meta-box.php
@@ -9,17 +9,18 @@ function glass_get_custom_field($value)
 {
     global $post;
 
-    $custom_field=get_post_meta($post->ID,$value,true);
-    if ( !empty( $custom_field ) )
+    $custom_field = get_post_meta($post->ID, $value, true);
+    if (!empty($custom_field))
         return is_array($custom_field) ? stripslashes_deep($custom_field) : stripslashes(wp_kses_decode_entities($custom_field));
     return false;
 }
 
 //---------------------------------------------
-function glass_add_custom_meta_box() {
+function glass_add_custom_meta_box()
+{
     add_meta_box(
         'glass-meta-box',
-        __('ویژگی و مشخصات فنی محصول','textdomain'),
+        __('ویژگی و مشخصات فنی محصول', 'textdomain'),
         'glass_meta_box_output',
         'post',
         'normal',
@@ -27,66 +28,120 @@ function glass_add_custom_meta_box() {
     );
 }
 
-add_action( 'add_meta_boxes', 'glass_add_custom_meta_box' );
+add_action('add_meta_boxes', 'glass_add_custom_meta_box');
 
 //------------------------------------------
 
-function glass_meta_box_output($post){
-    wp_nonce_field( 'my_glass_meta_box_nonce', 'glass_meta_box_nonce' );
-    echo 'لطفا توضیحات مربوط به ویژگی محصول رو در این بخش وارد نمایید .'?>
+function glass_meta_box_output($post)
+{
+    wp_nonce_field('my_glass_meta_box_nonce', 'glass_meta_box_nonce');
+    echo 'لطفا توضیحات مربوط به ویژگی محصول رو در این بخش وارد نمایید .' ?>
     <p>
-        <label for="glass1_textfield"><?php _e('نوع خودرو ', 'textdomain' ); ?>:</label>
-        <input type="text" name="glass1_textfield" id="glass1_textfield" value="<?php echo glass_get_custom_field( 'glass1_textfield' ); ?>" size="50" />
+        <label for="glass1_textfield"><?php _e('نوع خودرو ', 'textdomain'); ?>:</label>
+        <input type="text" name="glass1_textfield" id="glass1_textfield"
+               value="<?php echo glass_get_custom_field('glass1_textfield'); ?>" size="50"/>
     </p>
     <p>
-        <label for="glass2_textfield"><?php _e('ابعاد ( سانتیمتر ) ', 'textdomain' ); ?>:</label>
-        <input type="text" name="glass2_textfield" id="glass2_textfield" value="<?php echo glass_get_custom_field( 'glass2_textfield' ); ?>" size="50" />
+        <label for="glass2_textfield"><?php _e('ابعاد ( سانتیمتر ) ', 'textdomain'); ?>:</label>
+        <input type="text" name="glass2_textfield" id="glass2_textfield"
+               value="<?php echo glass_get_custom_field('glass2_textfield'); ?>" size="50"/>
     </p>
     <p>
-        <label for="glass3_textfield"><?php _e('جنس ', 'textdomain' ); ?>:</label>
-        <input type="text" name="glass3_textfield" id="glass3_textfield" value="<?php echo glass_get_custom_field( 'glass3_textfield' ); ?>" size="50" />
+        <label for="glass3_textfield"><?php _e('جنس ', 'textdomain'); ?>:</label>
+        <input type="text" name="glass3_textfield" id="glass3_textfield"
+               value="<?php echo glass_get_custom_field('glass3_textfield'); ?>" size="50"/>
     </p>
     <p>
-        <label for="glass4_textfield"><?php _e('وزن ', 'textdomain' ); ?>:</label>
-        <input type="text" name="glass4_textfield" id="glass4_textfield" value="<?php echo glass_get_custom_field( 'glass4_textfield' ); ?>" size="50" />
+        <label for="glass4_textfield"><?php _e('وزن ', 'textdomain'); ?>:</label>
+        <input type="text" name="glass4_textfield" id="glass4_textfield"
+               value="<?php echo glass_get_custom_field('glass4_textfield'); ?>" size="50"/>
     </p>
     <p>
-        <label for="glass5_textfield"><?php _e('رنگ ', 'textdomain' ); ?>:</label>
-        <input type="text" name="glass5_textfield" id="glass5_textfield" value="<?php echo glass_get_custom_field( 'glass5_textfield' ); ?>" size="50" />
+        <label for="glass5_textfield"><?php _e('رنگ ', 'textdomain'); ?>:</label>
+        <input type="text" name="glass5_textfield" id="glass5_textfield"
+               value="<?php echo glass_get_custom_field('glass5_textfield'); ?>" size="50"/>
     </p>
     <?php
 
 }
+
 //----------------------------------------------------
 function glass_meta_box_save($post_id)
 {
-    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-    if(!isset($_POST['glass_meta_box_nonce']) || !wp_verify_nonce($_POST['glass_meta_box_nonce'],'my_glass_meta_box_nonce')) return;
-    if(!current_user_can('edit_post')) return;
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+    if (!isset($_POST['glass_meta_box_nonce']) || !wp_verify_nonce($_POST['glass_meta_box_nonce'], 'my_glass_meta_box_nonce')) return;
+    if (!current_user_can('edit_post')) return;
 
-    if(isset($_POST['glass1_textfield']))
-        update_post_meta($post_id,'glass1_textfield',esc_attr($_POST['glass1_textfield']));
-    if(isset($_POST['glass1_textarea']))
-        update_post_meta( $post_id,'glass1_textarea', $_POST['glass1_textarea']);
+    if (isset($_POST['glass1_textfield']))
+        update_post_meta($post_id, 'glass1_textfield', esc_attr($_POST['glass1_textfield']));
+    if (isset($_POST['glass1_textarea']))
+        update_post_meta($post_id, 'glass1_textarea', $_POST['glass1_textarea']);
 
-    if(isset($_POST['glass2_textfield']))
-        update_post_meta($post_id,'glass2_textfield',esc_attr($_POST['glass2_textfield']));
-    if(isset($_POST['glass2_textarea']))
-        update_post_meta( $post_id,'glass2_textarea', $_POST['glass2_textarea']);
+    if (isset($_POST['glass2_textfield']))
+        update_post_meta($post_id, 'glass2_textfield', esc_attr($_POST['glass2_textfield']));
+    if (isset($_POST['glass2_textarea']))
+        update_post_meta($post_id, 'glass2_textarea', $_POST['glass2_textarea']);
 
-    if(isset($_POST['glass3_textfield']))
-        update_post_meta($post_id,'glass3_textfield',esc_attr($_POST['glass3_textfield']));
-    if(isset($_POST['glass3_textarea']))
-        update_post_meta( $post_id,'glass3_textarea', $_POST['glass3_textarea']);
+    if (isset($_POST['glass3_textfield']))
+        update_post_meta($post_id, 'glass3_textfield', esc_attr($_POST['glass3_textfield']));
+    if (isset($_POST['glass3_textarea']))
+        update_post_meta($post_id, 'glass3_textarea', $_POST['glass3_textarea']);
 
-    if(isset($_POST['glass4_textfield']))
-        update_post_meta($post_id,'glass4_textfield',esc_attr($_POST['glass4_textfield']));
-    if(isset($_POST['glass4_textarea']))
-        update_post_meta( $post_id,'glass4_textarea', $_POST['glass4_textarea']);
+    if (isset($_POST['glass4_textfield']))
+        update_post_meta($post_id, 'glass4_textfield', esc_attr($_POST['glass4_textfield']));
+    if (isset($_POST['glass4_textarea']))
+        update_post_meta($post_id, 'glass4_textarea', $_POST['glass4_textarea']);
 
-    if(isset($_POST['glass5_textfield']))
-        update_post_meta($post_id,'glass5_textfield',esc_attr($_POST['glass5_textfield']));
-    if(isset($_POST['glass5_textarea']))
-        update_post_meta( $post_id,'glass5_textarea', $_POST['glass5_textarea']);
+    if (isset($_POST['glass5_textfield']))
+        update_post_meta($post_id, 'glass5_textfield', esc_attr($_POST['glass5_textfield']));
+    if (isset($_POST['glass5_textarea']))
+        update_post_meta($post_id, 'glass5_textarea', $_POST['glass5_textarea']);
 }
-add_action('save_post','glass_meta_box_save');
+
+add_action('save_post', 'glass_meta_box_save');
+
+
+//=================================================
+//----------------- Single.php --------------------
+//=================================================
+?>
+
+<div style="padding-bottom: 8px;">
+    <i style="float: right;padding-left: 5px;" class="fa fa-truck"></i>
+    <p style="float: right;margin-bottom: 0;">
+        <strong>نوع خودرو
+            : </strong><?php echo get_post_meta(get_the_ID(), 'glass1_textfield', true); ?>
+    </p>
+</div>
+<br>
+<div style="padding-bottom: 8px;">
+    <i style="float: right;padding-left: 5px;" class="fa fa-scissors"></i>
+    <p style="float: right;margin-bottom: 0;">
+        <strong>ابعاد (سانتیمتر)
+            : </strong><?php echo get_post_meta(get_the_ID(), 'glass2_textfield', true); ?>
+    </p>
+</div>
+<br>
+<div style="padding-bottom: 8px;">
+    <i style="float: right;padding-left: 5px;" class="fa fa-tint"></i>
+    <p style="float: right;margin-bottom: 0;">
+        <strong>جنس
+            : </strong><?php echo get_post_meta(get_the_ID(), 'glass3_textfield', true); ?>
+    </p>
+</div>
+<br>
+<div style="padding-bottom: 8px;">
+    <i style="float: right;padding-left: 5px;" class="fa fa-globe"></i>
+    <p style="float: right;margin-bottom: 0;">
+        <strong>وزن
+            : </strong><?php echo get_post_meta(get_the_ID(), 'glass4_textfield', true); ?>
+    </p>
+</div>
+<br>
+<div style="padding-bottom: 25px;">
+    <i style="float: right;padding-left: 5px;" class="fa fa-eyedropper"></i>
+    <p style="float: right;margin-bottom: 0;">
+        <strong>رنگ
+            : </strong><?php echo get_post_meta(get_the_ID(), 'glass5_textfield', true); ?>
+    </p>
+</div><br>
