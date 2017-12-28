7c73a8ea5bae4ed6faf73e47bd3bc00cf5eb83f4
Mostafa Norzade <mostafanorzade@yahoo.com>
2017-12-28 16:19:15 +0330

    Create Random product.php

---
+++ Random product.php
@@ -0,0 +1,18 @@
+<div class="discount_other">
+    <div class="title_block"><span><?php echo opt('index_other_discounts'); ?></span></div>
+
+    <?php
+    $args = array(
+        'posts_per_page'   => 4,
+        'orderby'          => 'rand',
+        'post_type'        => 'product' ); 
+
+    $random_products = get_posts( $args );
+        
+        foreach ( $random_products as $post ) : setup_postdata( $post );
+            get_template_part('partials/content-product-mini');
+        endforeach; 
+        wp_reset_postdata();  
+    ?>
+    
+</div>
